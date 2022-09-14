39f149594fd4a73abe3809f9c4895529085d0970
Muhammad Rizky Al Farabi <59549551+rizkyfarabi03@users.noreply.github.com>
2022-09-14 23:10:24 +0800

    Tugas Pertama

---
+++ Tugas1_MuhammadRizkyAlFarabi_2110131310007.java
@@ -0,0 +1,9 @@
+public class Tugas1_MuhammadRizkyAlFarabi_2110131310007 {
+    public static void main(String[] args){
+    System.out.println("Nim     : 2110131310007");
+    System.out.println("Nama    : Muhammad Rizky Al Farabi");
+    System.out.println("Alamat  : JL.PERDAGANGAN KOMPLEK HKSN PERMAI BLOK 8A");
+    System.out.println("Hobi    : Main Futsal");
+    System.out.println("Mata Kuliah Yang Dikuasai : Pemograman Web");
+    }
+}
